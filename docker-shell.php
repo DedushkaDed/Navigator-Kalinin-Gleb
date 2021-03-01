85826f99a7fd6ead1dd93c77c1a6bdc99a552f78
Vladislav Timofeev <v.timofeev@iqdev.digital>
2021-03-01 21:12:06 +0300

    draft docker shell

--- docker-shell.php
+++ docker-shell.php
@@ -1,15 +1,40 @@
 <?php
-/**
- * 
- */
-
-require 'bootstrap.php';
-
-$containerName = $_ENV['PROJECT_NAME'] . '-php';
+error_reporting(0);
 
 $cmd = $argv[1];
 if (!$cmd) {
     die();
 }
-$cmd = "docker exec -it {$containerName} {$cmd}";
-echo shell_exec($cmd);
+
+try {
+    require 'bootstrap.php';
+    $containerName = $_ENV['PROJECT_NAME'] . '-php';
+    if (!$containerName) {
+        throw new Exception('Name not defined ENV');
+    }
+    execCommand($containerName, $cmd);
+} catch (Exception $e) {
+    $containerName = parsePsDockerCompose();
+    if (!$containerName) {
+        die();
+    }
+    execCommand($containerName, $cmd);
+}
+
+
+function execCommand($containerName, $cmd)
+{
+    $cmd = "docker exec -it {$containerName} {$cmd}";
+    echo shell_exec($cmd);
+    die();
+}
+
+function parsePsDockerCompose()
+{
+    $root = __DIR__;
+    $r = '/^.*-php/m';
+    $psResult = shell_exec("cd {$root} && docker-compose ps");
+    $matches = [];
+    preg_match($r, $psResult, $matches);
+    return current($matches);
+}
