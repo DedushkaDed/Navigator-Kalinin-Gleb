298c5abe801c68416a0dfab73648584fb73f2fd0
Vladislav Timofeev <v.timofeev@agimagroup.ru>
2021-04-03 09:16:41 +0300

    draft | phpcs

--- public/404.php
+++ public/404.php
@@ -1,17 +1,11 @@
-<?
-include_once($_SERVER['DOCUMENT_ROOT'].'/bitrix/modules/main/include/urlrewrite.php');
-
-CHTTP::SetStatus("404 Not Found");
-@define("ERROR_404","Y");
-
-require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
-
-$APPLICATION->SetTitle("404 Not Found");
-
-IQDEV\Base\Load::setTemplate('/local/markup/src/markup/page/errors/index.pug');
-
-if (CModule::IncludeModule('iqdev.options')){
-IQDEV\Base\Load::setProperty('errors', \iqdev\options\Options::getPageOptions('404_page'));
+<?php // phpcs:disable PSR1.Files.SideEffects.FoundWithSymbols
+if (!defined('ERROR_404')) {
+    define('ERROR_404', 'Y');
 }
+require($_SERVER['DOCUMENT_ROOT'].'/bitrix/header.php');
 
-require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");
+CHTTP::SetStatus('404 Not Found');
+
+$APPLICATION->SetTitle('404 Not Found');
+
+require($_SERVER["DOCUMENT_ROOT"].'/bitrix/footer.php');
