4b7d282e90a127e2b52d2dc7dabd577ae0d686f8
g.kalinin@iq-adv.ru <g.kalinin@iqdev.digital>
2021-04-22 12:56:55 +0500

    WIP: 2-aboutOnePart

--- public/about/index.php
+++ public/about/index.php
@@ -246,60 +246,63 @@ $aAboutOptions = \IQDEV\Options\Options::getPageOptions('about_page');
 <section class="section section--overflow-hidden mb-large">
     <div class="container">
         <div class="section-header">Нам доверяют</div>
-        <div class="slider-trust">
-            <div class="glide js-slider-trust">
-                <div class="glide__track" data-glide-el="track">
-                    <div class="glide__slides">
-                        <div class="glide__slide">
-                            <img class="img slider-trust__item" src="http://placehold.it/500x300/faa"/>
-                        </div>
-                        <div class="glide__slide">
-                            <img class="img slider-trust__item" src="http://placehold.it/500x300/afa"/>
-                        </div>
-                        <div class="glide__slide">
-                            <img class="img slider-trust__item" src="http://placehold.it/500x300/aaf"/>
-                        </div>
-                        <div class="glide__slide">
-                            <img class="img slider-trust__item" src="http://placehold.it/500x300/faf"/>
-                        </div>
-                        <div class="glide__slide">
-                            <img class="img slider-trust__item" src="http://placehold.it/500x300/ffa"/>
-                        </div>
-                    </div>
-                </div>
-                <div class="slider-trust__arrows">
-                    <div class="slider-trust-controls">
-                        <div data-glide-el="controls">
-                            <div class="slider-trust-controls__arrow glide__arrow glide__arrow--prev" data-glide-dir="&lt;">
-                                <svg width="22" height="24" viewBox="0 0 22 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                    <path
-                                        d="M0.939341 10.9393C0.353554 11.5251 0.353554 12.4749 0.939341 13.0607L10.4853 22.6066C11.0711 23.1924 12.0208 23.1924 12.6066 22.6066C13.1924 22.0208 13.1924 21.0711 12.6066 20.4853L4.12132 12L12.6066 3.51472C13.1924 2.92893 13.1924 1.97919 12.6066 1.3934C12.0208 0.807611 11.0711 0.807611 10.4853 1.3934L0.939341 10.9393ZM22 10.5L2 10.5V13.5L22 13.5V10.5Z"
-                                        fill="#D1D1D1"
-                                    ></path>
-                                </svg>
-                            </div>
-                        </div>
-                        <ul class="slider-trust-controls__list glide__bullets" data-glide-el="controls[nav]">
-                            <li class="glide__bullet" data-glide-dir="=0">0</li>
-                            <li class="glide__bullet" data-glide-dir="=1">1</li>
-                            <li class="glide__bullet" data-glide-dir="=2">2</li>
-                            <li class="glide__bullet" data-glide-dir="=3">3</li>
-                            <li class="glide__bullet" data-glide-dir="=4">4</li>
-                        </ul>
-                        <div data-glide-el="controls">
-                            <div class="slider-trust-controls__arrow glide__arrow glide__arrow--next" data-glide-dir="&gt;">
-                                <svg width="22" height="24" viewBox="0 0 22 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                    <path
-                                        d="M21.0607 13.0607C21.6464 12.4749 21.6464 11.5251 21.0607 10.9393L11.5147 1.3934C10.9289 0.807611 9.97919 0.807611 9.3934 1.3934C8.80761 1.97919 8.80761 2.92893 9.3934 3.51472L17.8787 12L9.3934 20.4853C8.80761 21.0711 8.80761 22.0208 9.3934 22.6066C9.97919 23.1924 10.9289 23.1924 11.5147 22.6066L21.0607 13.0607ZM0 13.5L20 13.5V10.5L0 10.5L0 13.5Z"
-                                        fill="#D1D1D1"
-                                    ></path>
-                                </svg>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
+        <?$APPLICATION->IncludeComponent(
+            "bitrix:news.list",
+            "about_trust_us",
+            Array(
+                "ACTIVE_DATE_FORMAT" => "d.m.Y",
+                "ADD_SECTIONS_CHAIN" => "N",
+                "AJAX_MODE" => "N",
+                "AJAX_OPTION_ADDITIONAL" => "",
+                "AJAX_OPTION_HISTORY" => "N",
+                "AJAX_OPTION_JUMP" => "N",
+                "AJAX_OPTION_STYLE" => "N",
+                "CACHE_FILTER" => "N",
+                "CACHE_GROUPS" => "N",
+                "CACHE_TIME" => "36000000",
+                "CACHE_TYPE" => "A",
+                "CHECK_DATES" => "Y",
+                "DETAIL_URL" => "",
+                "DISPLAY_BOTTOM_PAGER" => "N",
+                "DISPLAY_DATE" => "Y",
+                "DISPLAY_NAME" => "Y",
+                "DISPLAY_PICTURE" => "Y",
+                "DISPLAY_PREVIEW_TEXT" => "Y",
+                "DISPLAY_TOP_PAGER" => "N",
+                "FIELD_CODE" => array("PREVIEW_PICTURE", ""),
+                "FILTER_NAME" => "",
+                "HIDE_LINK_WHEN_NO_DETAIL" => "N",
+                "IBLOCK_ID" => "7",
+                "IBLOCK_TYPE" => "-",
+                "INCLUDE_IBLOCK_INTO_CHAIN" => "N",
+                "INCLUDE_SUBSECTIONS" => "N",
+                "MESSAGE_404" => "",
+                "NEWS_COUNT" => "10",
+                "PAGER_BASE_LINK_ENABLE" => "N",
+                "PAGER_DESC_NUMBERING" => "N",
+                "PAGER_DESC_NUMBERING_CACHE_TIME" => "36000",
+                "PAGER_SHOW_ALL" => "N",
+                "PAGER_SHOW_ALWAYS" => "N",
+                "PAGER_TEMPLATE" => ".default",
+                "PAGER_TITLE" => "Новости",
+                "PARENT_SECTION" => "",
+                "PARENT_SECTION_CODE" => "",
+                "PREVIEW_TRUNCATE_LEN" => "",
+                "PROPERTY_CODE" => array("", ""),
+                "SET_BROWSER_TITLE" => "N",
+                "SET_LAST_MODIFIED" => "N",
+                "SET_META_DESCRIPTION" => "N",
+                "SET_META_KEYWORDS" => "N",
+                "SET_STATUS_404" => "N",
+                "SET_TITLE" => "N",
+                "SHOW_404" => "N",
+                "SORT_BY1" => "SORT",
+                "SORT_BY2" => "SORT",
+                "SORT_ORDER1" => "DESC",
+                "SORT_ORDER2" => "ASC",
+                "STRICT_SECTION_CHECK" => "N"
+            )
+        );?>
     </div>
 </section>
 <section class="section mb-large">
