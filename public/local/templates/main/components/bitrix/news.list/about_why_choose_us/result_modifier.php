34bcb58a7406145c7e07ca449479843a408ce2ba
g.kalinin@iq-adv.ru <g.kalinin@iqdev.digital>
2021-05-20 11:42:22 +0500

    5-byuer | refactoring

--- public/local/templates/main/components/bitrix/news.list/about_why_choose_us/result_modifier.php
+++ public/local/templates/main/components/bitrix/news.list/about_why_choose_us/result_modifier.php
@@ -1,7 +1,7 @@
-<? if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED !== true) {
+<?
+if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED !== true) {
     die();
 }
-
 
 /** @var array $arParams */
 /** @var array $arResult */
