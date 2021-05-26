bc9917c5d59495ea0b001bcf1c61ee731a6c3004
g.kalinin@iq-adv.ru <g.kalinin@iqdev.digital>
2021-05-26 10:11:44 +0500

    4-aboutForms | refactoring

---
+++ public/local/components/iqdev/email.subscribe.additional/class.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace IQDEV\Components;
+
+class EmailSubscribeAdditional extends \CBitrixComponent
+{
+
+    /**
+     * Выполнение компонента
+     *
+     * @return void
+     */
+    public function executeComponent()
+    {
+        $this->includeComponentTemplate();
+    }
+}
